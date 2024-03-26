189beba09ec832cf020944b32c34521fb6c4c350
RITIK SAHU <Ritik2011071@akgec.ac.in>
2024-03-26 18:33:09 +0530

    Connection to mongodb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect('mongodb://localhost:27017').then(()=>{
+    console.log('Connected to mongodb');
+}).catch((err)=>{
+    console.log(err)
+})
 
 const app=express();
 
+
 app.listen(3000,()=>{
-    console.log("Server Running on 3000");
-})
+    
+    console.log("Server Running on 3000 ");
+}) 
